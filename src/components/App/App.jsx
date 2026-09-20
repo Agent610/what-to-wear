359252cfb54630b65a49685faf7f726bc9c6a43f
Agent610 <BhavsarParthD96@Gmail.com>
2026-09-20 19:14:57 -0400

    62%

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,6 +35,7 @@ function App() {
     });
   };
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
+  const [clothingItems, setClothingItems] = useState([]);
 
   const handleCardClick = (card) => {
     setActiveModal("preview");
